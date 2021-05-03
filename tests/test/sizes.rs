d7a5d3ef96eee871cf2709cfce85aa1eb78acbe1
Robin KAY <komadori@gekkou.co.uk>
2021-05-03 18:50:24 +0100

    Remove unnecessary casts from sizes test.

--- tests/test/sizes.rs
+++ tests/test/sizes.rs
@@ -7,7 +7,7 @@ use enumoid::Enumoid;
 
 #[test]
 fn test_zero() {
-  assert_eq!(<Zero as Enumoid>::SIZE, 0);
+  assert_eq!(Zero::SIZE, 0);
   assert_eq!(std::mem::size_of::<EnumFlags::<Zero>>(), 0);
   assert_eq!(std::mem::size_of::<EnumMap::<Zero, u8>>(), 0);
   assert_eq!(std::mem::size_of::<EnumOptionMap::<Zero, u8>>(), 0);
@@ -16,7 +16,7 @@ fn test_zero() {
 
 #[test]
 fn test_three() {
-  assert_eq!(<Three as Enumoid>::SIZE, 3);
+  assert_eq!(Three::SIZE, 3);
   assert_eq!(std::mem::size_of::<EnumFlags::<Three>>(), 1);
   assert_eq!(std::mem::size_of::<EnumMap::<Three, u8>>(), 3);
   assert_eq!(std::mem::size_of::<EnumOptionMap::<Three, u8>>(), 4);
@@ -25,7 +25,7 @@ fn test_three() {
 
 #[test]
 fn test_sixteen() {
-  assert_eq!(<Sixteen as Enumoid>::SIZE, 16);
+  assert_eq!(Sixteen::SIZE, 16);
   assert_eq!(std::mem::size_of::<EnumFlags::<Sixteen>>(), 2);
   assert_eq!(std::mem::size_of::<EnumMap::<Sixteen, u8>>(), 16);
   assert_eq!(std::mem::size_of::<EnumOptionMap::<Sixteen, u8>>(), 18);
@@ -34,7 +34,7 @@ fn test_sixteen() {
 
 #[test]
 fn test_seventeen() {
-  assert_eq!(<Seventeen as Enumoid>::SIZE, 17);
+  assert_eq!(Seventeen::SIZE, 17);
   assert_eq!(std::mem::size_of::<EnumFlags::<Seventeen>>(), 3);
   assert_eq!(std::mem::size_of::<EnumMap::<Seventeen, u8>>(), 17);
   assert_eq!(std::mem::size_of::<EnumOptionMap::<Seventeen, u8>>(), 20);
