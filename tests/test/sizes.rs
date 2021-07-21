0714387eecace302259e13b3bf920437e5007f11
Robin KAY <komadori@gekkou.co.uk>
2021-07-21 01:47:55 +0100

    Add large enum and more cases to sizes tests. Refactor asserts.

--- tests/test/sizes.rs
+++ tests/test/sizes.rs
@@ -1,42 +1,50 @@
-use crate::test::types::{Seventeen, Sixteen, Three, Zero};
+use crate::test::types::{Seventeen, Sixteen, Three, ThreeHundred, Zero};
+use enumoid::base::{EnumArrayHelper, EnumFlagsHelper};
 use enumoid::EnumFlags;
 use enumoid::EnumMap;
 use enumoid::EnumOptionMap;
 use enumoid::EnumVec;
-use enumoid::Enumoid;
+use enumoid::Size;
+
+fn test_type<T: EnumFlagsHelper + EnumArrayHelper<u8>>(
+  variants: usize,
+  value_bytes: usize,
+  word_bytes: usize,
+  flags_bytes: usize,
+) {
+  assert_eq!(T::SIZE, variants);
+  assert_eq!(std::mem::size_of::<T>(), value_bytes);
+  assert_eq!(std::mem::size_of::<Size<T>>(), word_bytes);
+  assert_eq!(std::mem::size_of::<EnumFlags<T>>(), flags_bytes);
+  assert_eq!(std::mem::size_of::<EnumMap<T, u8>>(), variants);
+  assert_eq!(
+    std::mem::size_of::<EnumOptionMap<T, u8>>(),
+    flags_bytes + variants
+  );
+  assert_eq!(std::mem::size_of::<EnumVec<T, u8>>(), word_bytes + variants);
+}
 
 #[test]
 fn test_zero() {
-  assert_eq!(Zero::SIZE, 0);
-  assert_eq!(std::mem::size_of::<EnumFlags::<Zero>>(), 0);
-  assert_eq!(std::mem::size_of::<EnumMap::<Zero, u8>>(), 0);
-  assert_eq!(std::mem::size_of::<EnumOptionMap::<Zero, u8>>(), 0);
-  assert_eq!(std::mem::size_of::<EnumVec::<Zero, u8>>(), 1);
+  test_type::<Zero>(0, 0, 1, 0);
 }
 
 #[test]
 fn test_three() {
-  assert_eq!(Three::SIZE, 3);
-  assert_eq!(std::mem::size_of::<EnumFlags::<Three>>(), 1);
-  assert_eq!(std::mem::size_of::<EnumMap::<Three, u8>>(), 3);
-  assert_eq!(std::mem::size_of::<EnumOptionMap::<Three, u8>>(), 4);
-  assert_eq!(std::mem::size_of::<EnumVec::<Three, u8>>(), 4);
+  test_type::<Three>(3, 1, 1, 1);
 }
 
 #[test]
 fn test_sixteen() {
-  assert_eq!(Sixteen::SIZE, 16);
-  assert_eq!(std::mem::size_of::<EnumFlags::<Sixteen>>(), 2);
-  assert_eq!(std::mem::size_of::<EnumMap::<Sixteen, u8>>(), 16);
-  assert_eq!(std::mem::size_of::<EnumOptionMap::<Sixteen, u8>>(), 18);
-  assert_eq!(std::mem::size_of::<EnumVec::<Sixteen, u8>>(), 17);
+  test_type::<Sixteen>(16, 1, 1, 2);
 }
 
 #[test]
 fn test_seventeen() {
-  assert_eq!(Seventeen::SIZE, 17);
-  assert_eq!(std::mem::size_of::<EnumFlags::<Seventeen>>(), 3);
-  assert_eq!(std::mem::size_of::<EnumMap::<Seventeen, u8>>(), 17);
-  assert_eq!(std::mem::size_of::<EnumOptionMap::<Seventeen, u8>>(), 20);
-  assert_eq!(std::mem::size_of::<EnumVec::<Seventeen, u8>>(), 18);
+  test_type::<Seventeen>(17, 1, 1, 3);
+}
+
+#[test]
+fn test_three_hundered() {
+  test_type::<ThreeHundred>(300, 2, 2, 38);
 }
